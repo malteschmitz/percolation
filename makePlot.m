160e331d007ee2eaf65c0363e157c543af09bc82
Malte Schmitz <malte@schmitz-sh.de>
2013-02-12 00:34:56 +0100

    Improves evaluation script

--- makePlot.m
+++ makePlot.m
@@ -1,18 +1,21 @@
 close all;
-colors_cluster = {'-r', '-k', '-b'};
-colors_infty = {'--r', '--k', '--b'};
-colors_infty_all = {':r', ':k', ':b'};
-Ls = [200];
+colors_cluster = {'-r', '-b', '-k'};
+colors_infty = {'--r', '--b', '--k'};
+colors_infty_all = {'-.r', '-.b', '-.k'};
+data = {'200-site', '200-bond'};
+legends = cell(1,length(data)*3);
 figure; hold on;
-for i = 1:length(Ls)
-    L = Ls(i);
-    values = load([num2str(L) '.txt']);
+for i = 1:length(data)
+    values = load([data{i} '.txt']);
     plot(values(1,:), values(2,:), colors_cluster{i});
+    legends(3*(i-1)+1) = {[data{i} ' p_{cluster}']};
     plot(values(1,:), values(3,:), colors_infty{i});
+    legends(3*(i-1)+2) = {[data{i} ' p_{\infty}']};
     plot(values(1,:), values(4,:), colors_infty_all{i});
+    legends(3*(i-1)+3) = {[data{i} ' p_{\infty/all}']};
 end
-title('site percolation');
-legend('200x200 p_{cluster}', '200x200 p_{infty}', '200x200 p_{infty/all}', 'Location', 'NorthWest');
+title('percolation');
+legend(legends, 'Location', 'SouthEast');
 xlabel('p');
 ylabel('propability');
 timesPlot;
